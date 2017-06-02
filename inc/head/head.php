4e5b397f4445923c891e526f3a3b1f4e9f1d9059
RômuloInnocêncio <romulo@ceicom.com.br>
2017-06-02 09:15:13 -0300

    re-structured

--- inc/head/head.php
+++
@@ -1,5 +0,0 @@
-<meta charset="utf-8">
-<meta http-equiv="X-UA-Compatible" content="IE=edge">
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<link rel="stylesheet" href="css/geral.css?v=<?php echo rand(5, 15) ?>">
-<link href="https://fonts.googleapis.com/css?family=Raleway:300,400,700" rel="stylesheet">
--- inc/system/connection.php
+++
@@ -1,5 +0,0 @@
-<?php 
-
-	$con = pg_connect("host=localhost port=5432 dbname=aula user=postgres password=123");
-
-?>
